f918c28ce728db6dfb12ab18beefce751398d348
singhpiyush998 <singhpiyush998@gmail.com>
2022-05-22 23:23:07 +0530

    Need for authentication before accessing the csv

--- src/github.js
+++ src/github.js
@@ -1,8 +1,10 @@
 "use strict";
 
 import { URLNotFoundError, AuthError } from "./errors.js";
-
-
+import axios from "axios";
+import fs from "fs"
+import "dotenv/config"
+import process from "node:process"
 const CLASSROOM_ASSIGNMENT_REGEX = /^.*\/classrooms\/(.+)\/assignments\/([\w\-\d\.]+)/;
 
 
@@ -27,9 +29,23 @@ class ClassroomAssignment {
         this.assignment = regex[2];
     }
 
-    download_roster_grades() {
+    async download_roster_grades() {
         var url = `https://classroom.github.com/classrooms/${this.classroom}/assignments/${this.assignment}/download_grades`;
-
-        // ...
+        // Need to download the file poiinted at that url
+        axios.defaults
+        try {
+            const response = await axios.get(url);
+            fs.writeFile("response.html", response.data, err => {
+                console.log(err);
+            })
+            /*
+            Tried to see what is in the response,it is the github classroom login page
+            We need to authenticate ourself before being able to download that CSV file
+            */
+        } catch (error) {
+            console.error(error);
+        } 
     }
 }
+
+new ClassroomAssignment("https://classroom.github.com/classrooms/91765210-python100/assignments/python101/", process.env.GITHUB_API_KEY).download_roster_grades()
