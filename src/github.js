7a9dbd2c03e4c16c7f220442fd51df57b57d29df
singhpiyush998 <singhpiyush998@gmail.com>
2022-05-23 01:28:54 +0530

    REMOVE: axios ADD: fetch

--- src/github.js
+++ src/github.js
@@ -1,7 +1,7 @@
 "use strict";
 
 import { URLNotFoundError, AuthError } from "./errors.js";
-import axios from "axios";
+import fetch from "node-fetch";
 import "dotenv/config"
 import process from "node:process"
 const CLASSROOM_ASSIGNMENT_REGEX = /^.*\/classrooms\/(.+)\/assignments\/([\w\-\d\.]+)/;
@@ -30,17 +30,19 @@ class ClassroomAssignment {
 
     async download_roster_grades() {
         var url = `https://classroom.github.com/classrooms/${this.classroom}/assignments/${this.assignment}/download_grades`;
-        let config = {
-            headers: {"Authorization": `token ${this.oauth_token}`}
-        }
         // Need to download the file poiinted at that url
-        axios.defaults
         try {
-            const response = await axios.get(url, config);
-            console.log(response.data); // The data is still clsasroom.github.com login page
-        } catch (error) {
-            console.error(error);
-        } 
+            const response = await fetch(url, {
+                method: "GET",
+                headers: {
+                    "Authorization": `token ${this.oauth_token}`
+                }
+            })
+            console.log(await response.text());
+        }
+        catch (error) {
+            console.log(error);
+        }
     }
 }
 
